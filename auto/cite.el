b698e64861714cf712e673515be143b084c0df9a
Jason Mansfield <trentonknight@gmail.com>
2011-12-17 10:43:28 -0500

    final push.

--- auto/cite.el
+++ auto/cite.el
@@ -5,5 +5,7 @@
      "curtis_colleen_president_2011"
      "white"
      "tritch_how_2011"
-     "reinhart2003debt")))
+     "reinhart2003debt"
+     "iscan2011productivity"
+     "arthur2011second")))
 
